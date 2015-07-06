038c7aee6a96b8e496f84bd3a7906cb92c856a3c
Alex Phillips <alexphillips@cbc-f2yrg3qd.cbc.corp>
2015-07-06 10:26:46 -0400

    fixed onAdError vs onError calls, also removed calling onAdError every time we incremented the counter

--- src/vast-vmap.js
+++ src/vast-vmap.js
@@ -417,15 +417,10 @@ function VASTAds(root, onAdsAvailable, onError, parentAd) {
 
   var onAdError = function () {
     onReceivedErrorCounter++;
-    if (that.onAdsError) {
-       var oae = that.onAdsError;
-       that.onAdsError = null;
-       oae.call();
-+    }
   }
 
   if (onReceivedErrorCounter == adElements.length) {
-    onAdError();
+    onError();
     return;
   }
 
